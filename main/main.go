b46bf3dad4a4e29d7244bf0ae661df09c06122b1
Vergiananta <christian.vergi@gmail.com>
2021-09-25 13:31:36 +0700

    [MUX]

---
+++ main/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func main()  {
+	r := mux.NewRouter()
+	r.HandleFunc("/handler", handler).Methods("GET")
+	http.Handle("/",r)
+	http.ListenAndServe(":8000", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request)  {
+	fmt.Fprint(w, "hello")
+}
