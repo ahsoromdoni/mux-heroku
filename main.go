3cd1e810e6f033db9536761409b3d6c07f375921
Ahmad Solehudin Romdoni <ahsoromdoni@gmail.com>
2021-09-27 13:47:54 +0700

    [FIX] HEROKU DEV

--- main.go
+++ main.go
@@ -2,17 +2,36 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/mux"
 	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
 )
 
-func main()  {
+func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/handler", handler).Methods("GET")
-	http.Handle("/",r)
-	http.ListenAndServe(":8000", nil)
+	r.HandleFunc("/ping", handler).Methods("GET")
+	http.Handle("/", r)
+	address := resolveAddress([]string{})
+	http.ListenAndServe(address, nil)
 }
 
-func handler(w http.ResponseWriter, r *http.Request)  {
+func resolveAddress(addr []string) string {
+	switch len(addr) {
+	case 0:
+		if port := os.Getenv("PORT"); port != "" {
+			// debugPrint("Environment variable PORT=\"%s\"", port)
+			return ":" + port
+		}
+		// debugPrint("Environment variable PORT is undefined. Using port :8080 by default")
+		return ":8080"
+	case 1:
+		return addr[0]
+	default:
+		panic("too many parameters")
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "hello")
 }
